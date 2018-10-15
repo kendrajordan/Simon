dae09d3ce82c63f4b015a27bba308d49dedfc7f0
ddshelt <ddshelton0001@gmail.com>
2018-10-15 17:07:14 -0400

    Active will be ready tomorrow

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
+Vue.component('sample', {
 
-var app = new Vue({
-  el:"#app",
-  data:{
-    buttons:['left','right','up','down'],
-  },
-  methods:{
-    game:function(){
-    let randomItem = this.buttons[Math.floor(Math.random()*this.buttons.length)];
-      console.log(randomItem);
+    props: ['colorclass','randomItem'],
+
+    data: function () {
+
+        return {
+            count: 0
+        }
+    },
+
+    template: '<button class="btn" v-bind:class="[ colorclass, (randomItem===colorclass)? \'active\' :\'\']" ></button>'
+
+});
+
+let app1 = new Vue({
+
+    el: "#app1",
+
+    data: {
+        message: "How are you today?",
+        colors: ['btn-danger', 'btn-warning', 'btn-success', 'btn-primary'],
+        randomItem:""
+    },
+
+    methods: {
+      game : function() {
+        // console.log(' I am here')
+      this.randomItem = this.colors[Math.floor(Math.random()*this.colors.length)];
+      // console.log(randomItem);
+      }
     }
 
-  }
-})
+});
