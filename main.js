7af311b2cf071f3174dcbc412a813a54b87fc2d3
Kendra Jordan <kendra.jordan8@gmail.com>
2018-10-15 14:01:45 -0400

    Made random array index

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 var app = new Vue({
   el:"#app",
   data:{
-    buttons:[left,right,up,down],
+    buttons:['left','right','up','down'],
   },
-  method:{
-    game:{
-      let randomItem = this.buttons[Math.floor(Math.random()*this.buttons.length)];
+  methods:{
+    game:function(){
+    let randomItem = this.buttons[Math.floor(Math.random()*this.buttons.length)];
       console.log(randomItem);
     }
 
